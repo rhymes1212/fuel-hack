7aeacd07091cd7c013280b1fa2ea87fabbc1abf1
BT Rhymes <138405011+rhymes1212@users.noreply.github.com>
2023-07-16 16:24:57 -0400

    Update charting.js

--- charting.js
+++ charting.js
@@ -1,46 +1,40 @@
-// import chart.js
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+// Alpha Vantage API Key
+const apiKey = "YOUR_ALPHA_VANTAGE_API_KEY"; // replace with your API key
 
-// prepare canvas for the chart
-<canvas id="myChart" width="400" height="400"></canvas>
+// Fetch data from the API
+fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        // Prepare data for chart
+        const stockData = data["Time Series (Daily)"];
+        const labels = Object.keys(stockData).reverse();
+        const values = labels.map(label => stockData[label]['4. close']);
 
-<script>
-    // your stock data
-    let stockData = [
-        { date: '2023-07-01', close: 100 },
-        { date: '2023-07-02', close: 102 },
-        { date: '2023-07-03', close: 105 },
-        // ...
-    ];
+        // Get the chart canvas
+        const ctx = document.getElementById('myChart').getContext('2d');
 
-    // prepare the data for the chart.js
-    let labels = stockData.map(d => d.date);
-    let data = stockData.map(d => d.close);
-
-    var ctx = document.getElementById('myChart').getContext('2d');
-
-    // create the chart
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Stock Price',
-                data: data,
-                borderColor: 'rgba(75, 192, 192, 0.2)',
-                backgroundColor: 'rgba(75, 192, 192, 0.05)',
-            }]
-        },
-        options: {
-            scales: {
-                x: {
-                    type: 'time',
-                    time: {
-                        unit: 'day'
+        // Create the chart
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'IBM Stock Price',
+                    data: values,
+                    borderColor: 'rgba(75, 192, 192, 0.2)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.05)',
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'day'
+                        }
                     }
                 }
             }
-        }
-    });
-</script>
-
+        });
+    })
+    .catch(error => console.error('Error:', error));
